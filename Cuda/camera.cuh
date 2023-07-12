1d69e388e14c9d6bd2de99272ceda3cf5a5c2187
Petrut Mit <petrutmit@gmail.com>
2023-07-12 12:36:15 +0300

    [CUDA] Made camera adjustable and explored different views

--- Cuda/camera.cuh
+++ Cuda/camera.cuh
@@ -2,24 +2,34 @@
 #define CAMERA_CUH
 
 #include "ray.cuh"
+#define PI 3.141592653589793f
 
 class camera {
     public:
-        __device__ camera() {
-            float aspect_ratio = 16.0f / 9.0f;
-            float viewport_height = 2.0f;
+        __device__ camera(
+            point3 lookfrom,
+            point3 lookat,
+            vec3 vup,
+            float vfov,
+            float aspect_ratio
+        ) {
+            float theta = degrees_to_radians(vfov);
+            float h = tan(theta / 2.0f);
+            float viewport_height = 2.0f * h;
             float viewport_width = aspect_ratio * viewport_height;
-            float focal_length = 1.0f;
 
-            origin = point3(0.0f, 0.0f, 0.0f);
-            horizontal = vec3(viewport_width, 0.0f, 0.0f);
-            vertical = vec3(0.0f, viewport_height, 0.0f);
-            lower_left_corner = origin - horizontal / 2.0f - vertical / 2.0f - vec3(0.0f, 0.0f, focal_length);
+            vec3 w = unit_vector(lookfrom - lookat);
+            vec3 u = unit_vector(cross(vup, w));
+            vec3 v = cross(w, u);
 
+            origin = lookfrom;
+            horizontal = viewport_width * u;
+            vertical = viewport_height * v;
+            lower_left_corner = origin - horizontal / 2.0f - vertical / 2.0f - w;
         }
 
-        __device__ ray get_ray(float u, float v) const {
-            return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+        __device__ ray get_ray(float s, float t) const {
+            return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
         }
 
     private:
@@ -27,6 +37,10 @@ class camera {
         point3 lower_left_corner;
         vec3 horizontal;
         vec3 vertical;
+
+    __device__ static float degrees_to_radians(float degrees) {
+        return degrees * PI / 180.0f;
+    }
 };
 
 #endif
