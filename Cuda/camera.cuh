ecb97efde54d557477fb213bbea44a7282c5b1d0
Petrut Mit <petrutmit@gmail.com>
2023-07-12 13:14:39 +0300

    [CUDA] Introduced depth-of-field

--- Cuda/camera.cuh
+++ Cuda/camera.cuh
@@ -2,7 +2,16 @@
 #define CAMERA_CUH
 
 #include "ray.cuh"
+#include <curand_kernel.h>
 #define PI 3.141592653589793f
+
+__device__ vec3 randomInUnitDisk(curandState *state) {
+    vec3 p;
+    do {
+        p = 2.0f * vec3(curand_uniform(state), curand_uniform(state), 0.0f) - vec3(1.0f, 1.0f, 0.0f);
+    } while (dot(p, p) >= 1.0f);
+    return p;
+}
 
 class camera {
     public:
@@ -11,25 +20,35 @@ class camera {
             point3 lookat,
             vec3 vup,
             float vfov,
-            float aspect_ratio
+            float aspect_ratio,
+            float aperture,
+            float focus_dist
         ) {
             float theta = degrees_to_radians(vfov);
             float h = tan(theta / 2.0f);
             float viewport_height = 2.0f * h;
             float viewport_width = aspect_ratio * viewport_height;
 
-            vec3 w = unit_vector(lookfrom - lookat);
-            vec3 u = unit_vector(cross(vup, w));
-            vec3 v = cross(w, u);
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
 
             origin = lookfrom;
-            horizontal = viewport_width * u;
-            vertical = viewport_height * v;
-            lower_left_corner = origin - horizontal / 2.0f - vertical / 2.0f - w;
+            horizontal = focus_dist * viewport_width * u;
+            vertical = focus_dist * viewport_height * v;
+            lower_left_corner = origin - horizontal / 2.0f - vertical / 2.0f - focus_dist * w;
+
+            lens_radius = aperture / 2.0f;
         }
 
-        __device__ ray get_ray(float s, float t) const {
-            return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
+        __device__ ray get_ray(float s, float t, curandState *state) const {
+            vec3 rd = lens_radius * randomInUnitDisk(state);
+            vec3 offset = u * rd.x() + v * rd.y();
+
+            return ray(
+                origin + offset,
+                lower_left_corner + s * horizontal + t * vertical - origin - offset
+            );
         }
 
     private:
@@ -37,6 +56,8 @@ class camera {
         point3 lower_left_corner;
         vec3 horizontal;
         vec3 vertical;
+        vec3 u, v, w;
+        float lens_radius;
 
     __device__ static float degrees_to_radians(float degrees) {
         return degrees * PI / 180.0f;
